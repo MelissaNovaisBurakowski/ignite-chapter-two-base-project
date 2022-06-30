e8439b3763172e23f3dc9e1e70d22b74584aaf6c
Mel Novais Burakowski <mf.novais.98@gmail.com>
2022-06-29 22:20:36 -0300

    feat: list categories created and docker added

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from "express";
-import { router } from "./routes"
+import { router } from "./routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
 const app = express();
 
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+console.log("eita 3");
 
 app.use(router);
 
-app.listen(3333, () => console.log("Server is runing"))
+app.listen(3333, () => console.log("Server is runing"));
