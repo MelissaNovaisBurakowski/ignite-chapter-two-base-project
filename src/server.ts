11f6d8d98908e1b295d35ccee1c5aa0ded1d387d
Mel Novais Burakowski <mf.novais.98@gmail.com>
2022-06-30 21:58:55 -0300

    feat: docker compose

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@ import express from "express";
 import { router } from "./routes";
 import swaggerUi from "swagger-ui-express";
 import swaggerFile from "./swagger.json";
+import "./database";
 const app = express();
 
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-console.log("eita 3");
 
 app.use(router);
 
